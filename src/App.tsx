530418874bc60e1a8008db8344f963b47db8da51
jacky <jacky@metallurgietech.com>
2022-10-25 08:44:19 +0800

    安裝tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App bg-black">
+      <header className="text-white">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
